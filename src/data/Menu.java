7c713722dba7fe13bac1b7b34ab236bd92e4f46f
DogOfTheBag <albercortijo@gmail.com>
2025-02-20 01:27:38 +0100

    Rematado
    Ejercicio terminado con comentarios

--- src/data/Menu.java
+++ src/data/Menu.java
@@ -25,7 +25,7 @@ public class Menu {
     }
     
     /*pequeño metodo que "limpia" la pantalla de la consola*/
-    private void CLS(){
+    public void CLS(){
         for (int i = 0; i < 50; i++) {
             System.out.println(" ");
             
@@ -37,7 +37,7 @@ public class Menu {
         Scanner sc = new Scanner(System.in);
         //convierto la eleccion a minuscula para que si el user pone S lo detecte como s y salga
         eleccion = Character.toLowerCase(eleccion);
-        if(empresas.size() > 0){
+        if(!empresas.isEmpty()){
             switch(eleccion){
                 //el caso 1 hace el gráfico de una empresa
                 case '1':
@@ -54,95 +54,76 @@ public class Menu {
                         Thread.sleep(1500);
                     }
                     return salir;
-                //sale del programa
-                case 's':
-                    System.out.println("Que tengas un buen dia!");
-                    salir = true;
-                    return salir;
-                //caso predeterminado en caso de que no se seleccione ninguna 
-                default:
-                    System.out.println("La introduccion introducida no es valida");
-                    Thread.sleep(1000);
-                    CLS();
-                    return salir;
+                    //sale del programa
+                    case 's':
+                        salir = true;
+                        return salir;
+                    //caso predeterminado en caso de que no se seleccione ninguna 
+                    default:
+                        System.out.println("La introduccion introducida no es valida");
+                        Thread.sleep(1000);
+                        CLS();
+                        return salir;
             }            
         }else{
             //en caso de no meter empresa te saldrá esto si o si
             System.out.println("No ha introducido ninguna empresa, saliendo del programa");
+            Thread.sleep(1500);
             salir = true;
             return salir;
         }
     }
-    /*está funcion se encargará de hacer un gráfico con los valores insertados por el user*/
+    /*está funcion se encarga de coger la empresa y asegurarse de que existe*/
     private void empresaGrafico(Scanner sc) throws InterruptedException {
-     System.out.println("Introduce la empresa sobre la que quieras ver el grafico");
-                System.out.println("");
-                for (Empresa empresa : empresas) {
-                    System.out.println("Empresa: " + empresa.getNombre());
-                }
-                String nombreEmpresa = sc.nextLine();
-                boolean encontrada = false;
-                
-                for (Empresa empresa : empresas) {
-                    if(nombreEmpresa.equalsIgnoreCase(empresa.getNombre())){
-                        generarGrafico(empresa,sc);
-                         Thread.sleep(3000);
-                        //TENGO QUE HACER LA FUNCION HACER GRAFICO
-                        encontrada = true;
-                    }
-                }
-                if (!encontrada)
-                    System.out.println("No se ha encontrado la empresa...");
-                
-    }
-    private void compararEmpresas(Scanner sc) throws InterruptedException{
-        System.out.println("Introduce las dos empresas que quieres comparar:");
+        System.out.println("Introduce la empresa sobre la que quieras ver el grafico");
+        System.out.println("");
         for (Empresa empresa : empresas) {
             System.out.println("Empresa: " + empresa.getNombre());
         }
-
-        System.out.print("Primera empresa: ");
-        String nombreEmpresa1 = sc.nextLine();
-        System.out.print("Segunda empresa: ");
-        String nombreEmpresa2 = sc.nextLine();
-
-        Empresa empresa1 = null;
-        Empresa empresa2 = null;
-        
+        String nombreEmpresa = sc.nextLine();
+        boolean encontrada = false;
+        //hacemos un for each para escribir todas las empresas
         for (Empresa empresa : empresas) {
-            if (nombreEmpresa1.equalsIgnoreCase(empresa.getNombre())) {
-                empresa1 = empresa;
-            }
-            if (nombreEmpresa2.equalsIgnoreCase(empresa.getNombre())) {
-                empresa2 = empresa;
+            /*Si la empresa introducida es igual a una de las introducidas antes, y si existe
+            hace el grafico*/
+            if(nombreEmpresa.equalsIgnoreCase(empresa.getNombre())){
+                generarGrafico(empresa,sc);
+                Thread.sleep(3000);
+                encontrada = true;
             }
         }
-        System.out.println("Grafico de la primera empresa:");
-        generarGrafico(empresa1,sc);
-        Thread.sleep(3000);
-        System.out.println("Grafico de la segunda empresa:");
-        generarGrafico(empresa2,sc);
-        Thread.sleep(3000);
+        if (!encontrada)
+            System.out.println("No se ha encontrado la empresa...");
+
     }
-    //TENGO QUE METERLE QUE HAGA GRAFICOS DE 3 Y 6 MESES
+   /*Método que se encarga de generar los graficos. Te pide primero la cantidad de meses
+    de los que quieres ver datos, y después lo representa. Explicaré el del año entero,
+    los demás son iguales, cambiando solo la cantidad de meses que se muestra*/
     private void generarGrafico(Empresa empresa, Scanner sc) {
         System.out.println("Introduce que datos quieres ver: (3) meses, (6) meses, (12) meses");
         int formato = sc.nextInt();
         switch(formato){
             case 12:
+                /*hacemos una matriz donde se haga el gráfico, las dimensiones en vertical
+                se mantienen siempre las mismas, y en horizontal los meses seleccionados + 1 para el nombre de la empresa*/
                 String[][] grafico = new String[12][13];
-        
+                //Ponemos espacios en blanco en todos los lados para que no salga nada null, por los huecos que no se rellenen después
                 for (int j = 0; j < 12; j++) {
                     for (int i = 0; i < 13; i++) {
                         grafico[j][i] = " ";
                     }
                 }
+                /*Este for funciona de la siguiente forma:
+                el limite dentro del for es 12 para que no se salga del limite marcado en el array, y que aparte no pase el valor (las cotizaciones de cada mes
+                después, se pondrán astericos verticalmente con la j, donde va de abajo a arriba. 
+                La dimension del array será 10 - j para señalizar que empieza desde la 10 (abajo) y va restando j para ir subiendo hasta el numero que sea*/
                 for (int i = 0; i < 12; i++) {
                     double valores[] = empresa.getCotizaciones();
                     for (int j = 0; j < valores[i] && j < 12; j++) {
                         grafico[10 - j][i] = "*";
                     }
-                }    
+                } 
+                //abajo pone los meses del año y el nombre de la empresa seleccionada
                 grafico[11][0] = "E";
                 grafico[11][1] = "F";
                 grafico[11][2] = "M";
@@ -156,7 +137,7 @@ public class Menu {
                 grafico[11][10] = "N";
                 grafico[11][11] = "D";
                 grafico[11][12] = "Empresa: " + empresa.getNombre();
-
+                // lo pintamos finalmente
                 for (int j = 0; j < 12; j++) {
                     for (int i = 0; i < 13; i++) {
                         System.out.print(grafico[j][i] + " ");
@@ -165,24 +146,22 @@ public class Menu {
                 }
                 break;
             case 6:
-                String[][] grafico6 = new String[12][7]; // La matriz tiene 12 filas y 7 columnas (6 meses + 1 para el nombre de la empresa)
+                //igual, pero en vez de 12 meses 6, por lo que la segunda dimension será 6 meses más el nombre de la empresa
+                String[][] grafico6 = new String[12][7]; 
 
-                // Inicializar la matriz con espacios vacíos
                 for (int j = 0; j < 12; j++) {
                     for (int i = 0; i < 7; i++) {
                         grafico6[j][i] = " ";
                     }
                 }
 
-                // Obtener los valores de cotización y agregar las barras en la matriz
                 double valores[] = empresa.getCotizaciones();
-                for (int i = 0; i < 6; i++) { // Limitar a los primeros 6 meses
+                for (int i = 0; i < 6; i++) { 
                     for (int j = 0; j < valores[i] && j < 12; j++) {
                         grafico6[10 - j][i] = "*";
                     }
                 }
 
-                // Agregar los nombres de los meses y el nombre de la empresa debajo de las barras
                 grafico6[11][0] = "E";
                 grafico6[11][1] = "F";
                 grafico6[11][2] = "M";
@@ -191,7 +170,6 @@ public class Menu {
                 grafico6[11][5] = "J";
                 grafico6[11][6] = "Empresa: " + empresa.getNombre();
 
-                // Mostrar la matriz
                 for (int j = 0; j < 12; j++) {
                     for (int i = 0; i < 7; i++) {
                         System.out.print(grafico6[j][i] + " ");
@@ -199,41 +177,65 @@ public class Menu {
                     System.out.println();
                 }
                 break;
-                case 3:
-                    String[][] grafico3 = new String[12][4]; // La matriz tiene 12 filas y 4 columnas (3 meses + 1 para el nombre de la empresa)
-
-                    // Inicializar la matriz con espacios vacíos
-                    for (int j = 0; j < 12; j++) {
-                        for (int i = 0; i < 4; i++) {
-                            grafico3[j][i] = " ";
-                        }
+            
+            case 3:
+                //segunda dimension 4, 3 meses mas el nombre de la empresa
+                String[][] grafico3 = new String[12][4]; 
+                for (int j = 0; j < 12; j++) {
+                    for (int i = 0; i < 4; i++) {
+                        grafico3[j][i] = " ";
                     }
-
-                    // Obtener los valores de cotización y agregar las barras en la matriz
-                    double valores3[] = empresa.getCotizaciones();
-                    for (int i = 0; i < 3; i++) { // Limitar a los primeros 3 meses
-                        for (int j = 0; j < valores3[i] && j < 12; j++) {
-                            grafico3[10 - j][i] = "*";
-                        }
+                }
+                double valores3[] = empresa.getCotizaciones();
+                for (int i = 0; i < 3; i++) { 
+                    for (int j = 0; j < valores3[i] && j < 12; j++) {
+                        grafico3[10 - j][i] = "*";
                     }
+                }
+                grafico3[11][0] = "E";
+                grafico3[11][1] = "F";
+                grafico3[11][2] = "M";
+                grafico3[11][3] = "Empresa: " + empresa.getNombre();
 
-                    // Agregar los nombres de los meses y el nombre de la empresa debajo de las barras
-                    grafico3[11][0] = "E";
-                    grafico3[11][1] = "F";
-                    grafico3[11][2] = "M";
-                    grafico3[11][3] = "Empresa: " + empresa.getNombre();
-
-                    // Mostrar la matriz
-                    for (int j = 0; j < 12; j++) {
-                        for (int i = 0; i < 4; i++) {
-                            System.out.print(grafico3[j][i] + " ");
-                        }
-                        System.out.println();
+                for (int j = 0; j < 12; j++) {
+                    for (int i = 0; i < 4; i++) {
+                        System.out.print(grafico3[j][i] + " ");
                     }
-    break;
-
-        
+                    System.out.println();
+                }
+            break;
         }
     }
-        
+        //metodo usado para poner 2 gráficos de 2 empresas al mismo tiempo
+        //es como el metodo graficoEmpresa pero para 2
+        private void compararEmpresas(Scanner sc) throws InterruptedException{
+        System.out.println("Introduce las dos empresas que quieres comparar:");
+        for (Empresa empresa : empresas) {
+            System.out.println("Empresa: " + empresa.getNombre());
+        }
+        //cogemos las 2 empresas que quiere usar el usuario
+        System.out.print("Primera empresa: ");
+        String nombreEmpresa1 = sc.nextLine();
+        System.out.print("Segunda empresa: ");
+        String nombreEmpresa2 = sc.nextLine();
+        //si pones una empresa que no está, directamente te corta el programa con una excepción
+        Empresa empresa1 = null;
+        Empresa empresa2 = null;
+        //busca entre las que está y si la encuentra la asigna, si no no.
+        for (Empresa empresa : empresas) {
+            if (nombreEmpresa1.equalsIgnoreCase(empresa.getNombre())) {
+                empresa1 = empresa;
+            }
+            if (nombreEmpresa2.equalsIgnoreCase(empresa.getNombre())) {
+                empresa2 = empresa;
+            }
+        }
+        //después usamos el metodo generarGráfico en ambos casos y ya
+        System.out.println("Grafico de la primera empresa:");
+        generarGrafico(empresa1,sc);
+        Thread.sleep(3000);
+        System.out.println("Grafico de la segunda empresa:");
+        generarGrafico(empresa2,sc);
+        Thread.sleep(3000);
+    } 
 }
--- src/data/main.java
+++ src/data/main.java
@@ -13,47 +13,63 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws InterruptedException {
+        /*Creamos unas cuantas variables necesarias, scanner, y una lista de empresas
+        con las que podremos usar metodos como add en un bucle posteriormente, que personalmente veo más sencillo*/
         boolean salir = false;
         Scanner sc = new Scanner(System.in);
         final int NUM_COTIZACIONES = 12;
         ArrayList<Empresa> empresas = new ArrayList<>();
+        //usamos el booleano salir para cerrar el bucle de introducción de datos de la empresa, si escribe parar cierra
         while(!salir){
             System.out.println("Introduce el nombre de la empresa(Escribe parar si no quieres introducir mas)");
             String nombre = sc.nextLine();
             if (nombre.equalsIgnoreCase("parar")){
                 salir = true;
             } else {
+                /*Si no escribe parar, creamos un array de cotizaciones, donde guardaremos los 12 datos del IBEX 35 de la empresa*/
                 double cotizacion[] = new double[NUM_COTIZACIONES];
                 for (int i = 0; i < NUM_COTIZACIONES ; i++) {
+                    /*Con una función que comprueba si el número se encuentra entre los que queremos, deja meter 12 datos double*/
                     System.out.println("Introduce 12 valores decimales (,) de las cotizaciones anuales de la empresa (0-10), Num " + (i+1));
                     boolean entradaValida = false;
+                    
                     while(!entradaValida){
-                    cotizacion[i] = sc.nextDouble();
-                    if(cotizacionCorrecta(cotizacion[i]))
-                        entradaValida = true;
-                       else {
-                        System.out.println("La entrada no es valida");
+                        cotizacion[i] = sc.nextDouble();
+                        if(cotizacionCorrecta(cotizacion[i]))
+                            entradaValida = true;
+                        else {
+                            System.out.println("La entrada no es valida");
                         }
                     }
                 }
+                //pongo aquí un nextline para que pille el buffer del anterior scanner y no se salte cosas
                 sc.nextLine();
+                //con el nombre y las cotizaciones, añadimos una nueva empresa a la lista.
                 empresas.add(new Empresa(nombre,cotizacion));
             }
         }
-       
+        /*Creamos un nuevo menu, que repetiremos hasta que el usuario nos indique que quiere salir*/
         salir = false;
         Menu m = new Menu(empresas);
         while(!salir){
             m.vis();
             System.out.println("Introduce la eleccion deseada");
+            /*En el enunciado nos dicen que las opciones tienen que ser 1, 2 y s
+            por lo que usaremos un char y lo compararemos después en la clase menu
+            
+            (usamos charAt(0) para indicar que cogemos el primer caracter de la String que nos pase
+            ya que scanner no deja coger chars, solo Strings.*/
             char eleccion = sc.next().charAt(0);
             salir = m.gestionarMenu(eleccion, salir);
+            m.CLS();
         }
+        System.out.println("Que tengas un buen dia!");
     }
+    //pequeño metodo que comprueba que el dato de la cotización está entre 0 y 10, si no no lo admite.
     public static boolean cotizacionCorrecta(double cotizacion){
         if (cotizacion >= 0 && cotizacion <= 10)
             return true;
-            else
-                return false;
+        else
+            return false;
     }
 }
